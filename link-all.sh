0e88a7acfd322f43a9cd6e7956c79ff430ef41e2
Binar Nugroho <binarnugroho775@gmail.com>
2024-03-31 16:01:23 +0700

    fix variable names

--- link-all.sh
+++ link-all.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-SC_PATH=$(realpath `dirname $0`)
+SCRIPT_PATH=$(realpath `dirname $0`)
 DOT_CONFIG=$HOME/.config
 
-CONFIG_FILE_DIR=$SC_PATH/config
-ln -sf $CONFIG_FILE_DIR/* $HOME/.config/
+CONFIG_FILE_DIR=$SCRIPT_PATH/config
+ln -sf $CONFIG_FILE_DIR/* $DOT_CONFIG/
 
 NVIM_LUA_DIR=$DOT_CONFIG/nvim/lua
-NVCHAD_CUSTOM_DIR=$SC_PATH/nvchad-custom
+NVCHAD_CUSTOM_DIR=$SCRIPT_PATH/nvchad-custom
 if [ ! -d $NVIM_LUA_DIR ]; then
   echo "nvim or nvchad not installed"
 else
@@ -18,7 +18,7 @@ else
   ln -sf $NVCHAD_CUSTOM_DIR $CUSTOM_DIR
 fi
 
-DOTFILE_HOME_DIR=$SC_PATH/dotfile-home
+DOTFILE_HOME_DIR=$SCRIPT_PATH/dotfile-home
 for f in $DOTFILE_HOME_DIR/*; do
   ln -sf $f "$HOME/.$(basename $f)"
 done
