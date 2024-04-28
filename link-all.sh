2ddd978662fb33db21554ad04267b7024e73b81f
Binar Nugroho <binarnugroho775@gmail.com>
2024-04-28 12:11:29 +0700

    update link-all.sh

--- link-all.sh
+++ link-all.sh
@@ -6,14 +6,6 @@ DOT_CONFIG="$HOME/.config"
 CONFIG_FILE_DIR=$SCRIPT_PATH/config
 ln -sf $CONFIG_FILE_DIR/* $DOT_CONFIG/
 
-NVIM_LUA_DIR="$DOT_CONFIG/nvim/lua"
-NVCHAD_CUSTOM_DIR="$SCRIPT_PATH/nvchad/lua/custom"
-if [ ! -d "$NVIM_LUA_DIR/core" ]; then
-  echo "nvim or nvchad not installed"
-else
-  ln -sf "$NVCHAD_CUSTOM_DIR" "$NVIM_LUA_DIR/"
-fi
-
 DOTFILE_HOME_DIR=$SCRIPT_PATH/dotfile-home
 for f in $DOTFILE_HOME_DIR/*; do
   DOT=$HOME/.$(basename $f)
