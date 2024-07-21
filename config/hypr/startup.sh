ec997284d47adf0ad6cf89fb340f816e2421b5d0
Binar Nugroho <binarnugroho775@gmail.com>
2024-07-21 15:20:46 +0700

    remove startup script

--- config/hypr/startup.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/bash
-
-for c in waybar hyprpaper dunst hypridle; do
-  "$c" 2>&1 >/dev/null &
-done
-
-dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP
-systemctl --user import-environment WAYLAND_DISPLAY XDG_CURRENT_DESKTOP
-notify-send "hello $(whoami)" -u low
